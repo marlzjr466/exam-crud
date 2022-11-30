cffc6adf4bbd78d17fff640a2fc6e257de8cb5eb
marlzjr466 <65201308+marlzjr466@users.noreply.github.com>
2022-11-30 23:35:26 +0800

    Update seeding.js

--- db/seeding.js
+++ db/seeding.js
@@ -1,34 +1,19 @@
 const fs = require('fs')
 
-const seeding = {
-    init: (connection, callback) => {
-        // Read SQL seed query
-        const seedQuery = fs.readFileSync('./db/crud.sql', {
-            encoding: 'utf-8'
-        });
-    
-        // check if seed already completed
-        connection.query('SHOW tables', (err, tables) => {
-            if (err) {
-                throw err;
-            }
-    
-            if (tables.length === 0) {
-                // Run seed query
-                connection.query(seedQuery, (error) => {
-                    if (error) {
-                        throw error;
-                    }
-    
-                    callback('SQL seed completed.');
-                    connection.end();
-                })
-            } else {
-                callback('SQL seed completed.')
-            }
-        })
-    }
-}
+const connection = require('./connection')
 
-module.exports = seeding
+connection.connect(()=> {
+    // Read SQL seed query
+    const seedQuery = fs.readFileSync('./db/crud.sql', {
+        encoding: 'utf-8'
+    });
+    // Run seed query
+    connection.query(seedQuery, (error) => {
+        if (error) {
+            throw error;
+        }
 
+        console.log('SQL seed completed.');
+        connection.end();
+    })
+})
